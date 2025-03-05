dff415007355c7df2a4beaef58afa847d1ceb37f
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-03-05 15:57:28 -0300

    Feat: adding kafkae vents configuration

---
+++ cotacao/cotacao/src/main/java/org/br/mineradora/message/KafkaEvents.java
@@ -0,0 +1,23 @@
+package org.br.mineradora.message;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.br.mineradora.dto.QuotationDTO;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class KafkaEvents {
+
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvents.class);
+
+    @Channel("quatation-channel")
+    Emitter<QuotationDTO> quotationDTOEmitter;
+
+    public void sendNewKafkaEvent(QuotationDTO quotation) {
+        LOG.info("Enviando cotação para tópicos do kafka ");
+        quotationDTOEmitter.send(quotation).toCompletableFuture().join();
+    }
+
+}
