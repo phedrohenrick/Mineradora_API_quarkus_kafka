33fe38a4e6164839ddc9a88b3353797ef298b8bc
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-03-07 15:56:20 -0300

    chore: remove unused import

--- cotacao/cotacao/src/main/java/org/br/mineradora/dto/QuotationDTO.java
+++ cotacao/cotacao/src/main/java/org/br/mineradora/dto/QuotationDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -15,6 +16,5 @@ import java.util.Date;
 public class QuotationDTO {
 
     private Date date;
-
     private BigDecimal currencyPrice;
 }
