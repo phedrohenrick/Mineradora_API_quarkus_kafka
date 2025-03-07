c64e7ab62f6622d463eec16506bc2f922c0f2191
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-03-07 15:57:37 -0300

    feat(config): fix a error from consuming external api. Added @JsonProperty("USDBRL") to fix the bug.

--- cotacao/cotacao/src/main/java/org/br/mineradora/dto/CurrencyPriceDTO.java
+++ cotacao/cotacao/src/main/java/org/br/mineradora/dto/CurrencyPriceDTO.java
@@ -1,15 +1,19 @@
 package org.br.mineradora.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Jacksonized
 @Data
 @Builder
 @AllArgsConstructor
+@RestClient
 public class CurrencyPriceDTO {
 
-    public USDBRL usdbrl;
+    @JsonProperty("USDBRL")
+    private USDBRL USDBRL;
 }
